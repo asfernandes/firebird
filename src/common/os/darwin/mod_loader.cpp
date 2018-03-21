9cd141262aea9a165cc1bc9c0768ee23f56e8392
Popa Marius Adrian <mapopa@gmail.com>
2018-03-21 16:56:49 +0200

    Update mod_loader.cpp

--- src/common/os/darwin/mod_loader.cpp
+++ src/common/os/darwin/mod_loader.cpp
@@ -98,13 +98,21 @@ ModuleLoader::Module* ModuleLoader::loadModule(const Firebird::PathName& modPath
 	void* module = dlopen(modPath.c_str(), FB_RTLD_MODE);
 	if (module == NULL)
 	{
-#ifdef DEBUG_LOADER
-		fprintf(stderr, "load error: %s: %s\n", modPath.c_str(), dlerror());
-#endif // DEBUG_LOADER
+#ifdef DEV_BUILD
+//		gds__log("loadModule failed loading %s: %s", modPath.c_str(), dlerror());
+#endif
 		return 0;
 	}
 
-	return FB_NEW_POOL(*getDefaultMemoryPool()) DlfcnModule(module);
+#ifdef DEBUG_THREAD_IN_UNLOADED_LIBRARY
+	Firebird::string command;
+	command.printf("echo +++ %s +++ >>/tmp/fbmaps;date >> /tmp/fbmaps;cat /proc/%d/maps >>/tmp/fbmaps",
+		modPath.c_str(), getpid());
+	system(command.c_str());
+#endif
+
+	return FB_NEW_POOL(*getDefaultMemoryPool()) DlfcnModule(*getDefaultMemoryPool(), modPath, module);
+
 }
 
 
